5767589b24ded047a8cc23d00b3d7e9811e43f26
Mark Melotto <markmelotto@gmail.com>
2025-09-08 11:48:37 +0200

    updated the branch for gitpuller

--- book/_static/custom_launch_button.js
+++ book/_static/custom_launch_button.js
@@ -47,7 +47,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     if (baseUrl.startsWith("https://")) {
       const repo = encodeURIComponent("https://github.com/eWaterCycle/getting-started");
-      const branch = "main";
+      const branch = "development";
       const notebookPath = "getting-started/book/content/first_model_run/first_run.ipynb"; // Change to your desired notebook
 
       const nbgitpullerUrl = `${baseUrl}/hub/user-redirect/git-pull?repo=${repo}&branch=${branch}&urlpath=lab/tree/${notebookPath}`;
